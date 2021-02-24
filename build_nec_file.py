ec5e82137d6e593deb568a46b5ad399a4ee097ef
Jacob Scott <jscott12009@gmail.com>
2021-02-24 03:07:34 -0800

    Add more customization arguments for script

--- build_nec_file.py
+++ build_nec_file.py
@@ -7,8 +7,6 @@ arguments, e.g. EX or RP, check out https://www.nec2.org/part_3/cards/
 from datetime import datetime as dt
 from math import *
 
-LIMS = [2, 5, 10, 20, 30, 40, 50, 60, 70, 80]
-
 
 def build_nec_file(
     comments,
@@ -18,6 +16,8 @@ def build_nec_file(
     excitations=[],
     rad_pattern=[],
     output="output",
+    lims=[2, 5, 10, 20, 30, 40, 50, 60, 70, 80],
+    sig_figs=2,
 ):
     """
     Creates a `.nec` file. The values can contain arithmetic in it. Anything
@@ -27,21 +27,32 @@ def build_nec_file(
     Parameters:
     comments - The comments that are found on CM cards, added as a list
     wires - The wire data found on GW cards, a list of lists where the
-            elements of the sublist are each parameter for the wire.
+        elements of the sublist are each parameter for the wire. Can use
+        constants defined in the `constants` argument and baisc arithmatic
+        (or any function defined in Python's `math` package).
     constants - A dictionary of constants to be substituted into the nec
-                file. Constant names may not be such that one is found
-                in another. For example, you cannot have 'offset' and
-                'origin_offset' because 'offset' can be found (via Python's
-                `replace` method in 'origin_offset')
-    frequency (default []) -
+        file. Constant names may not be such that one is found in another.
+        For example, you cannot have 'offset' and 'origin_offset' because
+        'offset' can be found (via Python's `replace` method in 'origin_offset').
+    frequency (default []) - Defines the FR card, the frequency range and step
+        for calculations.
     excitations (default []) - List for EX cards, cards that define excitations,
-                                e.g. voltage sources
+        e.g. voltage sources.
     rad_pattern (default []) - The RP card which defines how to calculate the
-                                the radiation pattern
-    output (default `output`) - The name of the output `.nec` file, the
-                                extension is automatically added
+        the radiation pattern.
+    output (default 'output') - The name of the output `.nec` file, the
+        extension is automatically added.
+    lims (default [2, 5, 10, 20, 30, 40, 50, 60, 70, 80]) - The character
+        number that each column ends on. For example, for the default,
+        we allocate 2 characters for the first argument (the card name),
+        3 for the next column, 5 for the third, and 10 for the rest.
+    sig_figs (default 2) - The number of significant figures used for the
+        numbers written in scientific notation (i.e. how many digits after
+        the decimal point).
     """
 
+    # scinot_ind tells this function at which column of a row to
+    # start using scientific notation
     def _format_rows(rows, card, scinot_ind):
         for row in rows:
             row_str = card
@@ -51,10 +62,10 @@ def build_nec_file(
                     param = param.replace(const_key, str(const_val))
 
                 # Add to line correctly formatted
-                rlim = LIMS[ind + 1] - LIMS[ind]
+                rlim = lims[ind + 1] - lims[ind]
                 if ind > (scinot_ind - 1):
                     # Change to 3-digit rounded scientific notation
-                    val = f"{eval(param):.2e}"
+                    val = f"{eval(param):.{sig_figs}e}"
                 else:
                     # Otherwise just evaluate, e.g. tag number
                     val = str(eval(param))
@@ -73,7 +84,7 @@ def build_nec_file(
     # Add wires
     _format_rows(rows=wires, card="GW", scinot_ind=2)
     # Wire end
-    nec_file.append("GE  0")
+    nec_file.append(f"GE{(lims[1] - lims[0] - 1)*' '}0")
     # Frequency
     _format_rows(rows=[frequency], card="FR", scinot_ind=4)
     # Excitations
